5346a260c221f8d8321fd27455c58fe71b095ee8
ahdiatahsan <ahdiatahsanaa@gmail.com>
2020-09-25 01:21:13 +0800

    add custom message in BotmanController

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use BotMan\BotMan\BotMan;
-use Illuminate\Http\Request;
 use App\Conversations\ExampleConversation;
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Messages\Attachments\File;
+use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BotManController extends Controller
 {
@@ -14,6 +17,22 @@ class BotManController extends Controller
     public function handle()
     {
         $botman = app('botman');
+
+        $botman->hears('/start|help|start|info|test' , function ($bot) {
+            $bot->reply(
+            <<<EOT
+            Hai saya adalah AdministrasiKDCW Chatbot, silahkan bertanya seputar administrasi di KeDai Computerworks dengan cara mengirim perintah seperti di bawah ini :
+            
+            /test [****]
+            Untuk menemukan informasi.
+EOT
+        );
+        });
+
+        //Fallback Error
+        $botman->fallback(function($bot) {
+            $bot->reply('Perintah yang anda input salah !, untuk mengetahui perintah yang ada silakan masukkan perintah /start atau help');
+        });
 
         $botman->listen();
     }
--- routes/botman.php
+++ routes/botman.php
@@ -1,9 +1,9 @@
 <?php
-use App\Http\Controllers\BotManController;
+// use App\Http\Controllers\BotManController;
 
-$botman = resolve('botman');
+// $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
-});
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
+// $botman->hears('Hi', function ($bot) {
+//     $bot->reply('Hello!');
+// });
+// $botman->hears('Start conversation', BotManController::class.'@startConversation');
