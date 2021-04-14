b673c1d1cbdd6091c3fa6b4db759cc01c6582dc4
ahdiatahsan <ahdiatahsanaa@gmail.com>
2021-04-14 23:29:46 +0800

    edit logpresensi controller

--- app/Http/Controllers/LogpresensiController.php
+++ app/Http/Controllers/LogpresensiController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Logpresensi;
+use App\Agenda;
+use App\Presensi;
+use App\User;
+use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
+use illuminate\Database\Eloquent\ModelNotFoundException;
 
 class LogpresensiController extends Controller
 {
@@ -28,6 +33,20 @@ class LogpresensiController extends Controller
     }
 
     /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function tambah($presensi)
+    {
+        $presensis = Presensi::where('id', '=', $presensi)->first();
+        $agendas = Agenda::where('id', '=', $presensis->agenda)->first();
+        $tanggal = (new Carbon($presensis->created_at))->isoFormat('dddd, D MMMM Y');
+        //dd($presensis);
+        return view('administrasi.logpresensi.create', compact('presensis', 'agendas', 'tanggal'));
+    }
+
+    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -35,7 +54,29 @@ class LogpresensiController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'noreg' => 'required|max:255',
+            'presensi' => 'required|max:255',
+            'agenda' => 'required|max:255',
+            'tanggal' => 'required|max:255'
+        ]);
+
+        try {
+            $noreg = User::select('id', 'noreg', 'nama')->where('noreg', '=', $request->noreg)->firstorFail();
+        } catch (ModelNotFoundException $err) {
+            return redirect()->route('logpresensi_tambah', $request->presensi)
+                ->with('not_found', 'No. Registrasi Anggota yang Anda masukkan tidak dapat ditemukan.');
+        }
+
+        Logpresensi::create([
+            'user' => $noreg['id'],
+            'presensi' => $request['presensi']
+        ]);
+
+        return redirect()->route('logpresensi_tambah', $request->presensi)
+            ->withInput()
+            ->with('success_absen', 'Presensi oleh ' . $noreg->nama . " (" . $noreg->noreg . ') pada agenda '
+                . $request->agenda . ' ( ' . $request->tanggal  . ' ) telah ditambahkan.');
     }
 
     /**
